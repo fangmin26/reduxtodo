e9c8e9d89d9b8980f730bcbbde0b27e2ef526f05
Hwang Minji <fangmin_@naver.com>
2021-08-10 10:30:10 +0900

    reduxtodo

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Route, BrowserRouter, Switch } from "react-router-dom";
+import { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
 
+const Todos = () => {
+  const dispatch = useDispatch();
+  const todos = useSelector((state) => state.todos);
+  const handleClick = (id) =>
+    dispatch({
+      type: "DELETE_TODO",
+      payload: id,
+    });
+  if (!todos || !todos.length) {
+    return <p>No Todos</p>;
+  }
+  return (
+    <ul>
+      {todos.map((todo) => (
+        <li onClick={() => handleClick(todo.id)}>{todo.label}</li>
+      ))}
+    </ul>
+  );
+};
+const TodoInput = () => {
+  const dispatch = useDispatch();
+  const [newTodo, setNewTodo] = useState();
+  const handleChange = (event) => setNewTodo(event.target.value);
+  const handleClick = () =>
+    dispatch({
+      type: "ADD_TODO",
+      payload: {
+        label: newTodo,
+        id: Math.ceil(Math.random() * 100),
+      },
+    });
+  return (
+    <>
+      <input value={newTodo} onChange={handleChange} type="text" />
+      <button onClick={handleClick}>ADD TODO</button>
+    </>
+  );
+};
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <header className="App-Header">Todos</header>
+      <Todos />
+      <TodoInput />
     </div>
   );
 }
